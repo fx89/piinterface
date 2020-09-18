43270424dd4b4216ec06877327cedcd44f94daaf
FX89 <catalin.ioan.georgescu@gmail.com>
2020-09-18 11:02:09 +0300

    Hello World REST API with Spring Boot

---
+++ piinterface-rest/src/main/java/com/desolatetimelines/piinterface/UiForPiApplication.java
@@ -0,0 +1,13 @@
+package com.desolatetimelines.piinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiForPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiForPiApplication.class, args);
+	}
+
+}
---
+++ piinterface-rest/src/test/java/com/desolatetimelines/piinterface/UiForPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desolatetimelines.piinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiForPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
