7426c61fa9c13272a9b6e7c27e21b9ed7c914308
fx89 <catalin.ioan.georgescu@gmail.com>
2020-09-29 10:30:28 +0300

    =Adding pin names

--- piinterface-pi/model/pin.py
+++ piinterface-pi/model/pin.py
@@ -3,8 +3,9 @@
 # Maps the pin's board id to its GPIO id
 # Used as a DTO - a list of available pins will be provided by the service as stated in the config file
 class Pin:
-    def __init__(self, boardId, gpioId, isSignalInverted, currentStatus):
+    def __init__(self, boardId, gpioId, isSignalInverted, currentStatus, name):
         self.boardId = boardId
         self.gpioId = gpioId
         self.currentStatus = currentStatus
         self.isSignalInverted = isSignalInverted
+        self.name = name
