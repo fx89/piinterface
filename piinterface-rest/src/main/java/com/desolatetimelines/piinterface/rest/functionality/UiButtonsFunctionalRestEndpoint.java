2eab9226f80384288afaf52f4f724310e8b0df3d
Catalin Ioan Georgescu <catalin.ioan.georgescu@gmail.com>
2020-10-16 19:03:02 +0300

    Removing unused import

--- piinterface-rest/src/main/java/com/desolatetimelines/piinterface/rest/functionality/UiButtonsFunctionalRestEndpoint.java
+++ piinterface-rest/src/main/java/com/desolatetimelines/piinterface/rest/functionality/UiButtonsFunctionalRestEndpoint.java
@@ -1,7 +1,5 @@
 package com.desolatetimelines.piinterface.rest.functionality;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
